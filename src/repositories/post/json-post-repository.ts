7e023c14aa34643b58f21a326d0c3c18fe181452
¡Duque <atanaelleonardo@gmail.com>
2025-05-23 13:00:24 -0300

    feat: create repositories to read database

---
+++ src/repositories/post/json-post-repository.ts
@@ -0,0 +1,41 @@
+import { resolve } from 'node:path'
+import { PostRepository } from './post-repository'
+import type { PostModel } from '@/models/post/post-model'
+import { readFile } from 'fs/promises'
+
+const ROOT_DIR = process.cwd
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR(),
+  'src',
+  'db',
+  'seed',
+  'posts.json',
+)
+
+export class JsonPostRepository implements PostRepository {
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8')
+    const parsedJson = JSON.parse(jsonContent)
+    const { posts } = parsedJson
+    return posts
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk()
+    return posts
+  }
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll()
+    const post = posts.find(post => post.id === id)
+
+    if (!post) throw new Error('Post inválido')
+
+    return post
+  }
+}
+
+export const postRepository: PostRepository = new JsonPostRepository()
+// ;(async () => {
+//   const posts = await postRepository.findAll()
+//   console.log(posts)
+// })()
