d0b72fac3250599ccc9875410a69d77c02969913
¡Duque <atanaelleonardo@gmail.com>
2025-05-23 17:23:40 -0300

    feat: added post repository and simulated delay in post retrieval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import { SpinLoader } from '@/components/SpinLoader'
+import { postRepository } from '@/repositories/post'
 
-export default function Home() {
+export default async function Home() {
+  const posts = await postRepository.findAll()
+
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <SpinLoader />
+      <header>
+        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a header</h1>
+      </header>
+      {posts.map(post => {
+        return <p key={post.id}>{post.title}</p>
+      })}
+
+      <footer>
+        <p className='text-6xl font-bold text-center py-8'>Aqui é a footer</p>
+      </footer>
     </div>
   )
 }
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -11,8 +11,17 @@ const JSON_POSTS_FILE_PATH = resolve(
   'seed',
   'posts.json',
 )
+const SIMULATE_WAIT_IN_MS = 5000
 
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return
+
+    await new Promise(resolve => {
+      setTimeout(resolve, SIMULATE_WAIT_IN_MS)
+    })
+  }
+
   private async readFromDisk(): Promise<PostModel[]> {
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8')
     const parsedJson = JSON.parse(jsonContent)
@@ -21,10 +30,14 @@ export class JsonPostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
+    await this.simulateWait()
+
     const posts = await this.readFromDisk()
     return posts
   }
   async findById(id: string): Promise<PostModel> {
+    await this.simulateWait()
+
     const posts = await this.findAll()
     const post = posts.find(post => post.id === id)
 
