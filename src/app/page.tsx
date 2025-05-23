6cb58f8d54c698afdef766a7066a220d8b442eb5
¡Duque <atanaelleonardo@gmail.com>
2025-05-23 17:27:47 -0300

    refactor Home component: replace direct post fetching with PostsList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import { postRepository } from '@/repositories/post'
+import { PostsList } from '@/components/PostsList'
+import { SpinLoader } from '@/components/SpinLoader'
+import { Suspense } from 'react'
 
 export default async function Home() {
-  const posts = await postRepository.findAll()
-
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <header>
         <h1 className='text-6xl font-bold text-center py-8'>Aqui é a header</h1>
       </header>
-      {posts.map(post => {
-        return <p key={post.id}>{post.title}</p>
-      })}
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
 
       <footer>
         <p className='text-6xl font-bold text-center py-8'>Aqui é a footer</p>
