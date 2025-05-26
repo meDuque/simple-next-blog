4ee3cf741dc19cf4da9719dcce76461213c5d411
¡Duque <atanaelleonardo@gmail.com>
2025-05-26 16:54:42 -0300

    refactor PostFeatured component, replacing removed components with new implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Container } from '@/components/Container'
 import { Header } from '@/components/Header'
-import { PostCoverImage } from '@/components/PostCoverImage'
-import { PostHeading } from '@/components/PostHeading'
+import { PostFeatured } from '@/components/PostFeatured'
 import { PostsList } from '@/components/PostsList'
 import { SpinLoader } from '@/components/SpinLoader'
 import { Suspense } from 'react'
@@ -12,38 +11,9 @@ export default async function Home() {
       <Container>
         <Header />
 
-        <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-          <PostCoverImage
-            linkProps={{ href: '#' }}
-            imageProps={{
-              src: '/images/bryen_5.png',
-              alt: 'Post cover image',
-              width: 1200,
-              height: 720,
-              priority: true,
-            }}
-          />
-
-          <div className='flex flex-col gap-4 sm:justify-center'>
-            <time
-              className='text-slate-600 block text-sm/tight'
-              dateTime='2025-05-23'
-            >
-              23/05/2025 10:00
-            </time>
-
-            <PostHeading as='h1' url='#'>
-              Delectus porro officiis
-            </PostHeading>
-
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut
-              inventore consectetur distinctio, maxime eaque libero temporibus
-              repudiandae nulla at. Delectus porro officiis, quam omnis libero
-              facilis. Dicta porro omnis dolor.
-            </p>
-          </div>
-        </section>
+        <Suspense fallback={<SpinLoader />}>
+          <PostFeatured />
+        </Suspense>
 
         <Suspense fallback={<SpinLoader />}>
           <PostsList />
--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,14 +1,48 @@
-import { ReactNode } from 'react';
+import { PostHeading } from '../PostHeading'
+import { PostCoverImage } from '../PostCoverImage'
 
-interface PostFeaturedProps {
-  children: ReactNode;
-}
+// interface PostFeaturedProps {
+//   children?: React.ReactNode
+// }
 
-export function PostFeatured({ children }: PostFeaturedProps) {
+export function PostFeatured() {
+  const slug = 'qualquer'
+  const postLink = `/post/${slug}`
+
   return (
     <>
-      <h1>PostFeatured</h1>
-      {children}
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <PostCoverImage
+          linkProps={{ href: postLink }}
+          imageProps={{
+            src: '/images/bryen_5.png',
+            alt: 'Post cover image',
+            width: 1200,
+            height: 720,
+            priority: true,
+          }}
+        />
+
+        <div className='flex flex-col gap-4 sm:justify-center'>
+          <time
+            className='text-slate-600 block text-sm/tight'
+            dateTime='2025-05-23'
+          >
+            23/05/2025 10:00
+          </time>
+
+          <PostHeading as='h1' url={postLink}>
+            Delectus porro officiis
+          </PostHeading>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut
+            inventore consectetur distinctio, maxime eaque libero temporibus
+            repudiandae nulla at. Delectus porro officiis, quam omnis libero
+            facilis. Dicta porro omnis dolor.
+          </p>
+        </div>
+      </section>
     </>
-  );
+  )
 }
