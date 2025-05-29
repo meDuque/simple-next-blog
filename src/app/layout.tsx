823ad3a7f27fb8288d60ade47c951112b4053a2e
¡Duque <atanaelleonardo@gmail.com>
2025-05-29 00:57:19 -0300

    feat: updated codebase with dark mode and typography plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='pt-BR'>
-      <body className={`antialiased`}>
+      <body className={`antialiased dark`}>
         <Container>
           <Header />
 
--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import ReactMarkdown from 'react-markdown'
 import rehypeSanitize from 'rehype-sanitize'
 import remarkGfm from 'remark-gfm'
@@ -9,7 +10,17 @@ interface SafeMarkdownProps {
 export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
   return (
     <>
-      <div>
+      <div
+        className={clsx(
+          'prose prose-slate w-full max-w-none overflow-hidden prose-a:transition prose-a:no-underline prose-a:text-blue-500 prose-a:hover:text-blue-700 prose-img:mx-auto md:prose-lg',
+          'prose-headings:text-slate-100',
+          'prose-p:text-slate-100',
+          'prose-strong:text-slate-100',
+          'prose-ul:text-slate-100',
+          'prose-td:text-slate-100',
+          'prose-code:text-slate-100',
+        )}
+      >
         <ReactMarkdown
           rehypePlugins={[rehypeSanitize]}
           remarkPlugins={[remarkGfm]}
